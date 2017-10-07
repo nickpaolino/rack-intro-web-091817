5419ef4db720ccc9c908b2c0acc5603ff7e5acb4
Nick Paolino <github email address>
2017-10-07 14:25:52 -0400

    Finished

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    name = "Nick"
+    resp.write "Hello, my name is #{name}"
     resp.finish
   end
 
 end
-
